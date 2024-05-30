6febcf0977edb37f694982884a14cd60c932681a
sosthene14 <sosthenemounsambote14@gmail.com>
2024-05-30 04:44:08 +0000

    update:updating main

--- src/components/main/userAds/UserAdsTraveler.tsx
+++ src/components/main/userAds/UserAdsTraveler.tsx
@@ -2,21 +2,20 @@ import { Footer } from "@/components/common/footer/Footer";
 import { NavBar } from "@/components/common/navbar/NavBar";
 
 interface ILabelProps {
-  htmlFor:string,
-  labelText:string,
-  value:string
+  htmlFor: string;
+  labelText: string;
+  value: string;
 }
-const Label = ({ htmlFor, labelText, value }:ILabelProps) => (
+const Label = ({ htmlFor, labelText, value }: ILabelProps) => (
   <div>
     <label
       htmlFor={htmlFor}
-      className="mb-2 flex justify-between text-sm font-medium text-gray-500 dark:text-gray-500"
+      className="mb-2 flex justify-between text-sm font-medium text-gray-500 dark:text-white"
     >
       {labelText}: <strong>{value}</strong>
     </label>
   </div>
 );
-
 
 const AdsPreview = () => {
   const datas = {
@@ -28,26 +27,33 @@ const AdsPreview = () => {
   };
 
   return (
-    <div
-      className="flex flex-col mx-auto w-96 py-7 rounded-2xl shadow-xl px-5"
-      style={{ backgroundColor: "#f8f9fa" }}
-    >
-      <p className="text-[12px] mb-5 text-red-500">Annonce non validée</p>
+    <div className="flex flex-col mx-auto w-96 py-7 dark:bg-slate-700 bg-slate-100  rounded-2xl shadow-xl px-5">
+      <p className="text-[15px] mb-5 text-rose-400">Annonce non validée</p>
       <Label htmlFor="name" labelText="Nom du voyageur" value={datas?.nom} />
-      <Label htmlFor="villeDepart" labelText="Ville de départ" value={datas?.villeDepartDoc?.name} />
-      <Label htmlFor="dateDepart" labelText="Date de départ" value={datas.dateDepart} />
-      <Label htmlFor="villeArrivee" labelText="Ville d'arrivée" value={datas?.villeArriveDoc?.name} />
-      <Label htmlFor="dateArrivee" labelText="Date d'arrivée" value={datas.dateArrivee} />
-      
+      <Label
+        htmlFor="villeDepart"
+        labelText="Ville de départ"
+        value={datas?.villeDepartDoc?.name}
+      />
+      <Label
+        htmlFor="dateDepart"
+        labelText="Date de départ"
+        value={datas.dateDepart}
+      />
+      <Label
+        htmlFor="villeArrivee"
+        labelText="Ville d'arrivée"
+        value={datas?.villeArriveDoc?.name}
+      />
+      <Label
+        htmlFor="dateArrivee"
+        labelText="Date d'arrivée"
+        value={datas.dateArrivee}
+      />
+
       <div className="flex gap-8 mt-5 justify-center">
-        <button
-          className="gradient-btn p-3 rounded-md"
-        >
-          Mettre à jour
-        </button>
-        <button
-          className="inline-flex items-center justify-center px-4 py-2 text-red-500 transition duration-300 ease-in-out bg-white border border-red-500 rounded-md shadow-md hover:bg-red-100 hover:text-red-600 focus:outline-none focus:ring focus:ring-red-200"
-        >
+        <button className="gradient-btn p-3 rounded-md">Mettre à jour</button>
+        <button className="inline-flex items-center justify-center px-4 py-2 text-red-500 transition duration-300 ease-in-out bg-white border border-red-500 rounded-md shadow-md hover:bg-red-100 hover:text-red-600 focus:outline-none focus:ring focus:ring-red-200">
           Supprimer
         </button>
       </div>
@@ -59,7 +65,9 @@ export const UserAdsTraveler = () => {
   return (
     <div>
       <NavBar />
-      <AdsPreview />
+      <div>
+        <AdsPreview />
+      </div>
       <Footer />
     </div>
   );
